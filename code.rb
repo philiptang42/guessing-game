f6c7185fb7451fc964724555e1f49eb8fbb76769
philiptang42 <philiptang42@gmail.com>
2015-11-15 16:09:19 -0500

    step 1 continuation

--- code.rb
+++ code.rb
@@ -1,3 +1,10 @@
 puts "Guessing Game time. What difficulty do you want to play at?"
 puts "Enter easy or hard"
 difficulty_answer = gets.chomp
+  if difficulty_answer == "easy"
+
+  elsif difficulty_answer == "hard"
+
+  else
+    puts "I'm going to ignore that. Now enter easy or hard."
+  end
